fa5ed1883978798f87360d6a56ecdf458bbc55f2
Evan Sparks <evan.c.sparks@gmail.com>
2017-06-05 01:11:13 -0400

    Config changes

--- sequencegen/image_downloader.py
+++ sequencegen/image_downloader.py
@@ -1,7 +1,7 @@
 import urllib.request
 import os.path
 import gzip
-import configparser
+import tempfile
 
 
 def _download_file(url, path):
@@ -12,44 +12,48 @@ def _download_file(url, path):
         urllib.request.urlretrieve(url, path)
 
 
+def get_label_file_path():
+    return os.path.join(tempfile.gettempdir(), "train-labels-idx1-ubyte.gz")
+
+
+def get_image_file_path():
+    return os.path.join(tempfile.gettempdir(), "train-images-idx3-ubyte.gz")
+
+
+def get_label_url():
+    return "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz"
+
+
+def get_image_url():
+    return "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz"
+
+
 class ImageDownloader:
     def __init__(self):
-        self.config = configparser.ConfigParser()
-        self.config.read(os.path.join(os.path.dirname(__file__), "config.ini"))
-        _download_file(self.get_image_url(), self.get_image_file_path())
-        _download_file(self.get_label_url(), self.get_label_file_path())
+        _download_file(get_image_url(), get_image_file_path())
+        _download_file(get_label_url(), get_label_file_path())
 
-    def get_image_url(self):
-        return self.config.get("ImageDownloader", "ImagesUrl")
-
-    def get_label_url(self):
-        return self.config.get("ImageDownloader", "LabelsUrl")
-
-    def get_image_file_path(self):
-        return self.config.get("ImageDownloader", "ImagesFilePath")
-
-    def get_label_file_path(self):
-        return self.config.get("ImageDownloader", "LabelsFilePath")
-
-    def get_image_bytes(self, start_offset, length):
+    @staticmethod
+    def get_image_bytes(start_offset, length):
         """
         Get a block of bytes from the digit image file.
         :param start_offset: The offset in the digit image file to start reading from.
         :param length: The length of the block of bytes to return.
         :return: A contiguous block of bytes.
         """
-        image_file = gzip.open(self.get_image_file_path())
+        image_file = gzip.open(get_image_file_path())
         image_file.seek(start_offset)
         image_bytes = image_file.read(length)
         image_file.close()
         return image_bytes
 
-    def get_labels(self):
+    @staticmethod
+    def get_labels():
         """
         Get the number of labels and label bytes from the labels file.
         :return: The number of labels in the file and a list of bytes of that length containing labels.
         """
-        label_file = gzip.open(self.get_label_file_path())
+        label_file = gzip.open(get_label_file_path())
         label_file.seek(4)
         num_labels = int.from_bytes(label_file.read(4), byteorder='big', signed=False)
         label_bytes = label_file.read(num_labels)
